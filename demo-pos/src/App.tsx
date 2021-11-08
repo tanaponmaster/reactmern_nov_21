555a7ea2f574d4e9cf9946c36ff7c865eb6d0b47
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-08 13:17:21 +0700

    1234

--- demo-pos/src/App.tsx
+++ demo-pos/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react";
+import LoginPage from "./LoginPage";
 
 // // const data = [1,2,3,4,5,6]
 // const data = [
@@ -24,7 +25,7 @@ export default function App({}: Props): ReactElement {
 
   return (
     <div>
-      <h1>1234</h1>
+      <LoginPage />
     </div>
   );
 }
