c22494347fc3d5590e7910855fb35bece76b5fa4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-08 13:11:15 +0700

    1234

--- demo-pos/src/App.tsx
+++ demo-pos/src/App.tsx
@@ -1,10 +1,25 @@
 import React, { ReactElement } from "react";
 
+// // const data = [1,2,3,4,5,6]
+// const data = [
+//   {title: 'angular', price: 100},
+//   {title: 'vuejs', price: 200},
+//   {title: 'react', price: 300},
+//   {title: 'react', price: "300", tag: 1},
+// ]
+
+interface Course {
+  title: string;
+  price: number;
+}
+
 interface Props {}
 
 export default function App({}: Props): ReactElement {
-  const data = [1,2,3];
-  
+  const data: Course[] = [
+    { title: "Angular", price: 100 },
+    { title: "React", price: 200 },
+  ];
 
   return (
     <div>
