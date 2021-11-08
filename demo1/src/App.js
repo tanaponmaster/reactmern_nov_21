0116b9b136e9253631f1e47534efda2b9064264a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-08 11:21:32 +0700

    1234

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -13,7 +13,7 @@ export default function App() {
     <div>
       {/* JSX */}
       <h1>{Math.random()}</h1>
-      {data.map(item=><h2>{item}</h2>)}
+      {data.map(item=><h2>{item.title}: {item.price}</h2>)}
     </div>
   )
 }
