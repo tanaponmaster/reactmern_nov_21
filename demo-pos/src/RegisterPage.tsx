a780aa273c94bd8c726cbf17614a029f4609a0d0
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-08 13:17:56 +0700

    1234

---
+++ demo-pos/src/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+export default function RegisterPage({}: Props): ReactElement {
+  return (
+    <div>
+      <h2>RegisterPage</h2>
+    </div>
+  );
+}
