bc15b4876db7118766bde740d6de436cfbb2a959
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-08 12:00:04 +0700

    1234

--- demo-pos/src/App.tsx
+++ demo-pos/src/App.tsx
@@ -3,6 +3,9 @@ import React, { ReactElement } from "react";
 interface Props {}
 
 export default function App({}: Props): ReactElement {
+  const data = [1,2,3];
+  
+
   return (
     <div>
       <h1>1234</h1>
