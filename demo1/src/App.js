e298093e8ef5e2e0b50cb0c24d07d3fc02d10e35
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-08 11:20:29 +0700

    1234

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 export default function App() {
 
-  const data = [1,2,3,4,5,6]
+  // const data = [1,2,3,4,5,6]
+  const data = [
+    {title: 'angular', price: 100},
+    {title: 'vuejs', price: 200},
+    {title: 'react', price: 300},
+  ]
 
   return (
     <div>
