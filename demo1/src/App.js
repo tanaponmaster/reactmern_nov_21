beeea178e32c85ef5b9ec2b83dd9185a6979d0c3
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-08 11:19:19 +0700

    1234

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 export default function App() {
+
+  const data = [1,2,3,4,5,6]
+
   return (
     <div>
       {/* JSX */}
       <h1>{Math.random()}</h1>
-      {[1,2,3,4].map(item=><h2>{item}</h2>)}
+      {data.map(item=><h2>{item}</h2>)}
     </div>
   )
 }
