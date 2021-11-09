8854a93974b434f215c5b6734f38eac028da8674
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 14:03:15 +0700

    1234

--- demo-pos/backend/api_auth.js
+++ demo-pos/backend/api_auth.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/login", (req, res) => {
+router.post("/login", (req, res) => {
   res.json({ result: "login" });
 });
 
 router.get("/register", (req, res) => {
   res.json({ result: "register" });
 });
-
 
 module.exports = router;
--- demo-pos/backend/server.js
+++ demo-pos/backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 app.use("/api/v2", require("./api"));
-// http://localhost:8081/images/9.jpg
 app.use(express.static(__dirname + "/uploaded"));
 
 app.listen(8081, () => {
