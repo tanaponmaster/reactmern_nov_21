c2609a9079e5dae1dd78ed2a844dbf9f3c1e79c4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 09:57:49 +0700

    1234

--- demo-pos/src/components/pages/LoginPage/LoginPage.tsx
+++ demo-pos/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,47 +1,14 @@
 import * as React from "react";
+import { Formik } from "formik";
 
 type LoginPageProps = {
   //
 };
 
 const LoginPage: React.FC<any> = () => {
-  // no side-effect
-  // let username = "lek";
-
-  // const [username, setUsername] = React.useState("");
-  // const [password, setPassword] = React.useState("");
-
-  interface Account {
-    username: string;
-    password: string;
-    level?: string;
-  }
-
-  const [account, setAccount] = React.useState<Account>({
-    username: "",
-    password: "",
-  });
-
   return (
     <div>
       <h1>Login</h1>
-      <input
-        type="text"
-        name="username"
-        id="username"
-        onChange={(e) => setAccount({ ...account, username: e.target.value })}
-      />
-      <br />
-      <input
-        type="text"
-        name="password"
-        id="password"
-        onChange={(e) => setAccount({ ...account, password: e.target.value })}
-      />
-      <br />
-      <span>#Debug: {JSON.stringify(account)}</span>
-      <br />
-      <button onClick={() => alert(JSON.stringify(account))}>Login</button>
     </div>
   );
 };
