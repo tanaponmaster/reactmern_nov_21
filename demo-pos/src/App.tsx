10a8a95e73667e461fdcf773967e299f974219ce
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 11:29:40 +0700

    1234

--- demo-pos/src/App.tsx
+++ demo-pos/src/App.tsx
@@ -1,22 +1,8 @@
-import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
+import { Container } from "@mui/material";
 import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
 import CssBaseline from "@mui/material/CssBaseline";
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import { styled } from "@mui/material/styles";
+import * as React from "react";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -24,65 +10,27 @@ import {
   RouteProps,
   Switch,
 } from "react-router-dom";
-import LoginPage from "./components/pages/LoginPage";
-import RegisterPage from "./components/pages/RegisterPage";
-import StockPage from "./components/pages/StockPage";
 import Header from "./components/layouts/Header";
 import Menu from "./components/layouts/Menu";
-
-const drawerWidth = 240;
-
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create("margin", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
+import ReportPage from "./components/pages/ReportPage";
+import ShopPage from "./components/pages/ShopPage";
+import StockCreatePage from "./components/pages/StockCreatePage";
+import StockEditPage from "./components/pages/StockEditPage";
+import StockPage from "./components/pages/StockPage";
+import TransactionPage from "./components/pages/TransactionPage";
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
 }));
 
-export default function PersistentDrawerLeft() {
-  const theme = useTheme();
+export default function App() {
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -96,6 +44,7 @@ export default function PersistentDrawerLeft() {
   // Protected Route
   const SecuredRoute = (props: RouteProps) => (
     <Route
+      {...props}
       render={(props) => (
         // ternary condition
         <Redirect to="/login" />
@@ -103,28 +52,55 @@ export default function PersistentDrawerLeft() {
     />
   );
 
+  // LoginRoute
+  const LoginRoute = (props: RouteProps) => (
+    <Route
+      {...props}
+      render={(props) => (
+        // ternary condition
+        <Redirect to="/stock" />
+      )}
+    />
+  );
+
   return (
-    <Router basename="">
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <Header open={open} handleDrawerOpen={handleDrawerOpen} />
-        <Menu open={open} handleDrawerClose={handleDrawerClose} />
-        <Main open={open}>
-          <DrawerHeader />
-          <div>
-            <Switch>
-              <Route path="/login" component={LoginPage} />
+    <Router
+      basename={process.env.REACT_APP_IS_PRODUCTION === "1" ? "/demo" : ""}
+    >
+      <Switch>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          {/* Header */}
+          <Header open={open} handleDrawerOpen={handleDrawerOpen} />
+          {/* Menu */}
+          <Menu open={open} handleDrawerClose={handleDrawerClose} />
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              p: 3,
+            }}
+          >
+            <Container>
+              <DrawerHeader />
+              {/* Pages Define */}
+              <LoginRoute path="/login" component={LoginPage} />
               <Route path="/register" component={RegisterPage} />
-              <SecuredRoute path="/stock" component={StockPage} />
+              <SecuredRoute path="/shop" component={ShopPage} />
+              <SecuredRoute exact={true} path="/stock" component={StockPage} />
+              <SecuredRoute path="/stock/create" component={StockCreatePage} />
+              <SecuredRoute path="/stock/edit/:id" component={StockEditPage} />
+              <SecuredRoute path="/report" component={ReportPage} />
+              <SecuredRoute path="/transaction" component={TransactionPage} />
               <Route
-                exact
+                exact={true}
                 path="/"
                 component={() => <Redirect to="/login" />}
               />
-            </Switch>
-          </div>
-        </Main>
-      </Box>
+            </Container>
+          </Box>
+        </Box>
+      </Switch>
     </Router>
   );
 }
