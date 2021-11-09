8716416d046462f6b8311cc2a7f3091bd7fd82ec
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 13:13:30 +0700

    1234

--- demo-pos/backend/server.js
+++ demo-pos/backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello 1234");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.end("I am root");
+});
+
+// http://localhost:8081/register?username=admin&password=1234
+app.get("/register", (req, res) => {
+  res.json({ register: "ok", input: req.query });
+});
+
+app.listen(8081, () => {
+  console.log("Server is running");
+});
