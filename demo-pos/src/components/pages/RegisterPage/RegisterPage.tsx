31232b8e0ded3278c53353dcf73173d9110a2e31
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 11:33:45 +0700

    1234

--- demo-pos/src/components/pages/RegisterPage/RegisterPage.tsx
+++ demo-pos/src/components/pages/RegisterPage/RegisterPage.tsx
@@ -1,45 +1,95 @@
-import * as React from "react";
+/* eslint-disable import/no-anonymous-default-export */
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
 import { Formik } from "formik";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { SxProps } from "@mui/system";
 
-type RegisterPageProps = {
-  //
+const classes: SxProps = {
+  root: { display: "flex", justifyContent: "center", alignItems: "center" },
+  submitBtn: { marginTop: 4 },
+  canelBtn: { marginTop: 2 },
 };
 
-const RegisterPage: React.FC<any> = () => {
-  return (
-    <div>
-      <h1>Register</h1>
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={(values, { setSubmitting }) => {
-          alert(JSON.stringify(values));
-        }}
-      >
-        {(props) => (
-          <form onSubmit={props.handleSubmit}>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              onChange={props.handleChange}
-              value={props.values.username}
-            />
-            <br />
-            <input
-              type="text"
-              name="password"
-              id="password"
-              onChange={props.handleChange}
-              value={props.values.password}
-            />
+export default (props: any) => {
+  const showForm = ({
+    values,
+    handleChange,
+    handleSubmit,
+    setFieldValue,
+    isSubmitting,
+  }: any) => {
+    return (
+      <form noValidate onSubmit={handleSubmit}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          onChange={handleChange}
+          value={values.username}
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          onChange={handleChange}
+          value={values.password}
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button
+          sx={classes.submitBtn}
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          disabled={isSubmitting}
+        >
+          Create
+        </Button>
+        <Button
+          sx={classes.canelBtn}
+          onClick={() => props.history.goBack()}
+          type="button"
+          fullWidth
+          variant="outlined"
+        >
+          Cancel
+        </Button>
+      </form>
+    );
+  };
 
-            <br />
-            <button>Submit</button>
-          </form>
-        )}
-      </Formik>
-    </div>
+  const initialValue: any = { username: "", password: "" };
+  return (
+    <Box sx={classes.root}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Register
+          </Typography>
+          <Formik
+            initialValues={initialValue}
+            onSubmit={(values, { setSubmitting }) => {}}
+          >
+            {showForm}
+          </Formik>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
-
-export default RegisterPage;
