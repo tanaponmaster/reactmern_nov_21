39fc181a11d178dd4b14963eb52c506d881da1f2
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 13:54:12 +0700

    1234

--- demo-pos/backend/server.js
+++ demo-pos/backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 app.use("/api/v2", require("./api"));
+// http://localhost:8081/images/9.jpg
+app.use(express.static(__dirname + "/uploaded"));
 
 app.listen(8081, () => {
   console.log("Server is running");
