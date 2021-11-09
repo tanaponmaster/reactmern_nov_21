55d06c69ff48645fb6961bceda9fde9234ba3bdd
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 14:13:44 +0700

    1234

--- demo-pos/backend/api_auth.js
+++ demo-pos/backend/api_auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/login", (req, res) => {
-  res.json({ result: "login" });
+  res.json({ result: "login", echo: req.body });
 });
 
 router.get("/register", (req, res) => {
--- demo-pos/backend/server.js
+++ demo-pos/backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-app.use("/api/v2", require("./api"));
+
 app.use(express.static(__dirname + "/uploaded"));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use("/api/v2", require("./api"));
 
 app.listen(8081, () => {
   console.log("Server is running");
