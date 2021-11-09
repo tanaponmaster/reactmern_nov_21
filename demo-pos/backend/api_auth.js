ec769c45515fb3a3743ccb290544f580686db109
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 16:01:45 +0700

    1234

--- demo-pos/backend/api_auth.js
+++ demo-pos/backend/api_auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const Users = require("./models/user_schema");
 
 router.post("/login", (req, res) => {
   res.json({ result: "login", echo: req.body });
 });
 
 router.post("/register", (req, res) => {
+  Users.create(req.body);
   res.json({ result: "register", echo: req.body });
 });
 
