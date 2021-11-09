b0f26b41aeec569271ee105199f1b460ced40bbe
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 13:44:04 +0700

    1234

--- demo-pos/backend/api.js
+++ demo-pos/backend/api.js
@@ -9,4 +9,14 @@ router.get("/register", (req, res) => {
   res.json({ result: "register" });
 });
 
+
+router.get("/product", (req, res) => {
+    res.json({ result: "product" });
+  });
+
+
+router.get("/transaction", (req, res) => {
+    res.json({ result: "transaction" });
+  });
+
 module.exports = router;
