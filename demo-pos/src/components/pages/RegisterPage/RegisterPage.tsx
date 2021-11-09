4a8fed6d44503ade449a641d361ed644079fee02
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-09 09:50:56 +0700

    1234

--- demo-pos/src/components/pages/RegisterPage/RegisterPage.tsx
+++ demo-pos/src/components/pages/RegisterPage/RegisterPage.tsx
@@ -1,47 +1,43 @@
 import * as React from "react";
+import { Formik } from "formik";
 
 type RegisterPageProps = {
   //
 };
 
 const RegisterPage: React.FC<any> = () => {
-  // no side-effect
-  // let username = "lek";
-
-  // const [username, setUsername] = React.useState("");
-  // const [password, setPassword] = React.useState("");
-
-  interface Account {
-    username: string;
-    password: string;
-    level?: string;
-  }
-
-  const [account, setAccount] = React.useState<Account>({
-    username: "",
-    password: "",
-  });
-
   return (
     <div>
       <h1>Register</h1>
-      <input
-        type="text"
-        name="username"
-        id="username"
-        onChange={(e) => setAccount({ ...account, username: e.target.value })}
-      />
-      <br />
-      <input
-        type="text"
-        name="password"
-        id="password"
-        onChange={(e) => setAccount({ ...account, password: e.target.value })}
-      />
-      <br />
-      <span>#Debug: {JSON.stringify(account)}</span>
-      <br />
-      <button onClick={() => alert(JSON.stringify(account))}>Login</button>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        {(props) => (
+          <form onSubmit={props.handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              onChange={props.handleChange}
+              value={props.values.username}
+            />
+            <br />
+            <input
+              type="text"
+              name="password"
+              id="password"
+              onChange={props.handleChange}
+              value={props.values.password}
+            />
+
+            <br />
+            <button>Submit</button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 };
