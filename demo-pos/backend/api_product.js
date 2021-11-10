a7253accf3490a566406641ddef400a46cdebe81
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-10 11:27:19 +0700

    1234

--- demo-pos/backend/api_product.js
+++ demo-pos/backend/api_product.js
@@ -4,7 +4,7 @@ const Products = require("./models/product_schema");
 const { tokenIntercept1, tokenIntercept2 } = require("./demo_intercept");
 
 // http://localhost:8081/api/v2/product?token1=1234&token2=4321
-router.get("/product", tokenIntercept1, tokenIntercept2, async (req, res) => {
+router.get("/product", async (req, res) => {
   const result = await Products.find();
   res.json(result);
 });
