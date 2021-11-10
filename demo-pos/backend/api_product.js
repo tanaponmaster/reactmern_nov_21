52ac788b3cab4849d50771a5e8458820c8866fc2
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-10 13:44:59 +0700

    1234

--- demo-pos/backend/api_product.js
+++ demo-pos/backend/api_product.js
@@ -41,4 +41,18 @@ router.delete("/product/id/:id", async (req, res) => {
   res.json({ result: "ok", message: JSON.stringify(doc) });
 });
 
+// Add Product
+router.post("/product", async (req, res) => {
+  try {
+    var form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+      let doc = await Products.create(fields); // insert
+      // await uploadImage(files, doc); // save image
+      res.json({ result: "ok", message: JSON.stringify(doc) }); // reply result
+    });
+  } catch (err) {
+    res.json({ result: "nok", message: JSON.stringify(err) });
+  }
+});
+
 module.exports = router;
