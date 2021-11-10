25745722808ea9380eb7741ce12ab917747bd2c6
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-10 10:08:48 +0700

    1234

--- demo-pos/backend/api_auth.js
+++ demo-pos/backend/api_auth.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const Users = require("./models/user_schema");
 var bcrypt = require("bcryptjs");
 
-router.post("/login", (req, res) => {
-  res.json({ result: "login", echo: req.body });
+router.post("/login", async (req, res) => {
+  let doc = await Users.findOne({ username: req.body.username });
+  res.json({ result: "login", echo: doc });
 });
 
 // db.users.find().pretty()
