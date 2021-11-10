8f5551edd53964983d7852a5d2db3312d4157fea
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-10 11:16:26 +0700

    1234

--- demo-pos/backend/demo_intercept.js
+++ demo-pos/backend/demo_intercept.js
@@ -1,9 +1,17 @@
-const tokenIntercept = (req, res, next) => {
-  if (req.query.token == "1234") {
+const tokenIntercept1 = (req, res, next) => {
+  if (req.query.token1 == "1234") {
     next();
   } else {
-    res.end("invalid token");
+    res.end("invalid token 1");
   }
 };
 
-module.exports = tokenIntercept;
+const tokenIntercept2 = (req, res, next) => {
+  if (req.query.token2 == "4321") {
+    next();
+  } else {
+    res.end("invalid token 2");
+  }
+};
+
+module.exports = { tokenIntercept1, tokenIntercept2 };
