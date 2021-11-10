25142c22ca1a2534f47f80ac489339ca222c4b7c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-11-10 11:10:01 +0700

    1234

--- demo-pos/backend/api_product.js
+++ demo-pos/backend/api_product.js
@@ -2,26 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Products = require("./models/product_schema");
 
+const tokenIntercept = (req, res, next) => {
+  if (req.query.token == "1234") {
+    next();
+  } else {
+    res.end("invalid token");
+  }
+};
+
 // http://localhost:8081/api/v2/product
-router.get("/product", async (req, res) => {
+router.get("/product", tokenIntercept, async (req, res) => {
   const result = await Products.find();
   res.json(result);
 });
 
 // intercept demo
 // http://localhost:8081/api/v2/test?token=1234
-router.get(
-  "/test",
-  (req, res, next) => {
-    if (req.query.token == "1234") {
-      next();
-    } else {
-      res.end("invalid token");
-    }
-  },
-  (req, res) => {
-    res.json({ result: "pass intercept" });
-  }
-);
+router.get("/test", tokenIntercept, (req, res) => {
+  res.json({ result: "pass intercept" });
+});
 
 module.exports = router;
